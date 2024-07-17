306114211a70036bc7c9fa7ec9ea1fa03f88f1f2
Aryan-1289 <davariyaaryan33@gmail.com>
2024-07-17 10:29:50 +0530

    Add files via upload

---
+++ Encryption of Ceaser Cipher.py
@@ -0,0 +1,39 @@
+# Code For Encryption
+
+def encrypt(string, shift):
+
+  cipher = ''
+  for char in string: 
+    if char == ' ':
+      cipher = cipher + char
+    elif  char.isupper():
+      cipher = cipher + chr((ord(char) + shift - 65) % 26 + 65)
+    else:
+      cipher = cipher + chr((ord(char) + shift - 97) % 26 + 97)
+  
+  return cipher
+
+text = input("Enter Word: ")
+s = int(input("Enter Shift Number: "))
+print("Original String: ", text)
+print("After Encryption: ", encrypt(text, s))
+
+# Code For Decryption
+
+def decrypt(string, shift):
+
+  cipher = ''
+  for char in string: 
+    if char == ' ':
+      cipher = cipher + char
+    elif  char.isupper():
+      cipher = cipher + chr((ord(char) + shift - 65) % 26 + 65)
+    else:
+      cipher = cipher + chr((ord(char) + shift - 97) % 26 + 97)
+  
+  return cipher
+
+text = input("Enter Word: ")
+s = -int(input("Enter Shift Number: "))
+print("Original String: ", text)
+print("After Decryption: ", decrypt(text, s))
